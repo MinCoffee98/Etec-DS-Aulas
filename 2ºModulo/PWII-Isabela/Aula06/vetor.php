26bbd0ce59a05ee70a07944a6d21bafd01e8c8ec
MinCoffee98 <isa98vih@gmail.com>
2022-09-02 22:33:53 -0300

    Laço de Repetição

---
+++ 2ºModulo/PWII-Isabela/Aula06/vetor.php
@@ -0,0 +1,23 @@
+<?php
+   
+
+     echo "<br />";
+     $nota = array();
+     $nota[0] = 8;
+     $nota[1] = 2;
+     $nota[2] = 9;
+     $nota[3] = 1;
+
+     $media = ($nota[0]+ $nota [1] + $nota [2] + $nota [3]) / 4;
+        echo "Sua média é $media"
+    
+?>
+
+
+
+
+*//3 - Criar uma tabela HTML com 10 linhas contendo as colunas Produto e Valor. Utilizando laço de repetição exiba as linha ímpares na cor azul e as pares na cor vermelha.
+
+4 - Criar uma tabela HTML com 10 linhas contendo as colunas Produto e Valor. Exiba apenas na tabela os produtos cujo valor seja inferior a 1000.
+
+5 - Utilizando vetores e laços de repetição, crie uma página que exiba a imagem referente a um país, seu nome e o seu continente. //*
