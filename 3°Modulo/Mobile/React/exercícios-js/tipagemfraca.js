7c6a113c8132e734851b7ec4abb098fca92f1777
MinCoffee98 <isa98vih@gmail.com>
2023-02-13 22:24:01 -0300

    Aula de JavaScript

---
+++ 3°Modulo/Mobile/React/exercícios-js/tipagemfraca.js
@@ -0,0 +1,13 @@
+let qualquer = ["Daniel", "Isa"]
+let qualquer2 = {
+    nome: "Joao",
+    idade: 35
+}
+console.log(qualquer)
+console.log(Array.isArray(qualquer))
+console.log(qualquer2)
+console.log(typeof qualquer2)
+
+qualquer = 3.14
+console.log (qualquer)
+console.log (typeof qualquer) //tipagem fraca significa que a variavel tem 
