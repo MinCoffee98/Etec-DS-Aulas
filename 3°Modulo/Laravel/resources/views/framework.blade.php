3bb01d9bbbcae42f1be9bf374723c0f00696233e
MinCoffee98 <isa98vih@gmail.com>
2023-02-16 22:28:15 -0300

    Aula de laravel

---
+++ 3°Modulo/Laravel/resources/views/framework.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+   
+<h2>O que é um framework?</h2>
+    
+    <p> 
+        Framework é um termo inglês que, em sua tradução direta, significa estrutura. 
+        De maneira geral, essa estrutura é <br>  feita para resolver um problema específico. </p>
+
+    <b>Na programação, um framework é um conjunto de códigos genéricos capaz de unir trechos de um projeto de desenvolvimento. </b>
+
+    <p>Ele funciona como uma peça de quebra cabeça capaz de se encaixar nos mais diversos lugares <br>
+     e conectar todos as linhas de código de uma maneira quase perfeita. </p>
+    
+     <div>
+        <h3> 4 Vantagens da utilização do framework</h3>
+
+        <ul>
+            <li> Segurança </li>
+            <p> Ao ser largamente utilizada e testada, a framework vai melhorando o seu desempenho na área da segurança. Quando é detectado um problema ou uma falha, rapidamente a comunidade de utilizadores alerta ou corrige as falhas de segurança, <br>
+            beneficiando todos os utilizadores da framework. </p>
+        
+            <li> Integração </li>
+            <p>É provável que um projeto não seja concluído apenas com uma framework e que seja necessário integrar diferentes plataformas. <br>
+            Grande parte das frameworks são pensadas para integrarem e comunicarem com diferentes sistemas. <br>
+            Por exemplo, a framework desenvolvida pela 
+            Opensoft – Lightweightform – para o desenvolvimento de formulários, permite selecionar qualquer tipo de tecnologia para a recolha dos dados obtidos a partir de um formulário.</p>
+        
+            <li> Usabilidade</li>
+            <p>Quando falamos de frameworks específicas para front-end, temos a garantia de que os elementos são desenvolvidos tendo em conta princípios de usabilidade que permitem uma ótima experiência de utilização. Ao ser muito utilizada, qualquer problema <br>
+            que a framework tenha é facilmente detectada e corrigida. </p>
+            
+            <li> Eficiência </li>
+            <p>O processo de desenvolvimento da aplicação é mais fácil, porque não é necessário escrever tantas linhas de código como nos projetos desenvolvidos de raiz.<br>
+            As frameworks disponibilizam bibliotecas e funcionalidades que permitem poupar tempo e esforço. <br>
+            Já lá vai o tempo em que eram necessárias horas e centenas de linhas de código para construir uma funcionalidade simples. Há ainda a vantagem de poder reutilizar o código produzido em projetos futuros. </p>
+        </ul>
+    </div>                                                                                                                                                                                             
+</body>
+</html>
---
+++ 3°Modulo/Laravel/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/contato', function () {
+    return view('contato');
+});
+
+
+Route::get('/quem-somos', function () {
+    return view('quem-somos');
+});
+
+Route::get('/framework', function () {
+    return view('framework');
+});
